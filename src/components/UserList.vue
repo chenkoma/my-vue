30aba69c7dbafcf6e737c57ed1178be94e8a0ad2
Koma <koma.chen@yuansuan.com>
2023-09-07 10:52:13 +0800

    first commit

---
+++ src/components/UserList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>test</h2>
+    <table>
+      <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+export default {
+  name: 'UserList',
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    Vue.http.get('/demo/find?value=0')
+      .then(response => {
+        this.users = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import UserList from '@/components/UserList'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/list',
+      name: 'UserList',
+      component: UserList
+    }
+  ]
+})
